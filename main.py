cb51a66308e53f732b6de380f3c93fc3bdd65b89
C-Loewenich <103130767+C-Loewenich@users.noreply.github.com>
2023-07-01 12:51:25 +0200

    Refactoring done

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import shutil
 from zipfile import ZipFile
 
 root_path = os.getcwd()
-cache_path = os.path.join(root_path, "files/cache")
-zip_path = os.path.join(root_path, "files/data.zip")
+cache_path = os.path.join(root_path, "files\cache")
+zip_path = os.path.join(root_path, "files\data.zip")
 
+print(cache_path)
 
 def clean_cache():
     #Creates the cache folder and if it already exist catch the error in stead clear the folder.
-    try:
+    """ try: #This is a good optoin if it is important to keep the folder.
         os.mkdir(cache_path)
         print("Cache folder created")
     except FileExistsError:
@@ -25,7 +26,12 @@ def clean_cache():
             try:
                 shutil.rmtree(path_to_delete)
             except OSError:
-                os.remove(path_to_delete)
+                os.remove(path_to_delete) """
+    if os.path.exists(cache_path): #this is a better option if the folder does not spelicit needs remain a part of the tree.
+        print("Cache exist - Cache directory deleted")
+        shutil.rmtree(cache_path)
+    os.mkdir(cache_path)
+    print("Cache directory created")
 
 
 def cache_zip(zip_path, cache_path):
@@ -37,7 +43,6 @@ def cache_zip(zip_path, cache_path):
 
 def cached_files():
     #Lists the files in the cache.
-    cache_path = r"C:\Users\Casper\WincBackend\files\cache"
     absolute_path_list = []
     for file in os.listdir(cache_path):
         absolute_path_list.append(os.path.join(cache_path, file))
@@ -46,22 +51,16 @@ def cached_files():
 
 def find_password(file_list):
     #Finds the document that contains the word password and extracts the actual password from the string.
-    ## I choose to do it this way because i did not want to expect that there was an ":" after the password. With this option the only recuriment is that the actual password is stated after the word "password". but this option is of course a few more lines of code.
     search_string = "password"
     for file_path in file_list:
         try:
-            file = open(file_path)
-            text = file.read()
-            file.close()
-            index = text.find(search_string)
-            if index >= 0:
-                text_lines = text.splitlines()
-                for line in text_lines:
+            with open(file_path) as file:
+                for line in file:
                     if search_string in line:
                         words = line.split(" ")
-                        for i in range(len(words)):
-                            if search_string in words[i]:
-                                return words[i+1]
+                        for i in range(len(words)): #iterates each word in string
+                            if search_string in words[i]: #if the searchstring is in that word (this way it has no influence if the current word is password, password: or password=)
+                                return words[i+1].replace("\n", "") #it returns the next word in the string and remove an possible line break.
         except:
             print("An error occured!")
     return "no password found"
